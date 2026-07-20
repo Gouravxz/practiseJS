6a6a47b9078a96588c52d5996e58bdd369b98a29
Gourav <Gouravxzx@gmail.com>
2026-07-20 16:22:00 +0530

    l2next

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
+//practice java script print and alert
 alert("Welcome to our website");
 console.log("hello world");
 console.log("welcome");
 console.log("wow how's the css");
+// variables
+name = "Gourav in";
+age = 43;
+isFollow = true;
+number = 30;
+// now print
+console.log(name);
