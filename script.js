d6d794ecb837f05501e1140ab22bfec9f30b65a6
Gourav <Gouravxzx@gmail.com>
2026-07-22 19:58:43 +0530

    Add various console logs and variable examples

--- script.js
+++ script.js
@@ -39,3 +39,32 @@ const sentence = "JavaScript is a very powerful programming language";
 const result = findLongestWord(sentence);
 
 console.log("The longest word is: " + result); // Output: programming
+//practice java script print and alert
+console.log("welcome");
+console.log("wow how's the css");
+// variables
+name = "Gourav in";
+age = 43;
+isFollow = true;
+number = 30;
+// now print
+console.log(name);
+// now prc null and undefined
+name = undefined;
+age = null;
+// so thsese are samee values but when i change tha variable.....
+fullName = "mohan dash"
+dob =  ("19/06/2025");
+console.log(fullName);
+console.log(dob);
+//today i learn oprators
+let a = 90;
+let b = 90;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+let score = 90;
+console.log(score);
+
