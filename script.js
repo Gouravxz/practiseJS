4003d0406298722e1591e56294096d61ae9913d3
Gourav <Gouravxzx@gmail.com>
2026-07-23 16:57:24 +0530

    Add various console logs and variable examples

--- script.js
+++ script.js
@@ -45,3 +45,53 @@ const profile = {
 }
 console.log(profile);
 
+//practice java script print and alert
+console.log("welcome");
+console.log("wow how's the css");
+// variables
+name1 = "Gourav in";
+age = 43;
+isFollow = true;
+number = 30;
+// now print
+console.log(name1);
+// now prc null and undefined
+age = null;
+// so thsese are samee values but when i change tha variable.....
+fullName = "mohan dash"
+dob =  ("19/06/2025");
+console.log(fullName);
+console.log(dob);
+//store data
+let item1Price = 300;
+let item2Price = 400;
+let discount = 100;
+const costmerName = "amitkumar";
+let total = item1Price + item2Price;
+let finalBill = total - discount;
+console.log("final bill is: ", finalBill);
+console.log("your total bill without discount is;", total);
+console.log("name:",name , "tshirt:", item1Price , "shirt:", item2Price , "discount on:", discount , "your total bill:", total , "final pay bill:", finalBill);
+let product = {
+    itemName: "Pen",
+    rate: 99,
+    isAvalibe: true,
+    discount: 9,
+}
+let pay = product.rate - product.discount;
+console.log("details:", product);
+console.log(product.rate);
+console.log("finalpay", pay);
+// create a const object called profile
+const profile = {
+    name: "Gourav",
+    post: 256,
+    follower: 1000,
+    isFollow: false,
+}
+console.log(profile);
+
+let name3 = "john";
+let lastname = "dey"
+let fullName1 = name3+" "+lastname;
+console.log("fullname is:", fullName1);
