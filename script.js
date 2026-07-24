4debac7b9decee46057f8035d9a5f94fdd88fdab
Gourav <Gouravxzx@gmail.com>
2026-07-24 14:55:41 +0530

    Refactor script.js and add loop examples
    Refactored variable declarations and added loops for calculations.

--- script.js
+++ script.js
@@ -2,53 +2,6 @@
 console.log("welcome");
 console.log("wow how's the css");
 // variables
-name = "Gourav in";
-age = 43;
-isFollow = true;
-number = 30;
-// now print
-console.log(name);
-// now prc null and undefined
-name = undefined;
-age = null;
-// so thsese are samee values but when i change tha variable.....
-fullName = "mohan dash"
-dob =  ("19/06/2025");
-console.log(fullName);
-console.log(dob);
-//store data
-let item1Price = 300;
-let item2Price = 400;
-let discount = 100;
-const costmerName = "amitkumar";
-let total = item1Price + item2Price;
-let finalBill = total - discount;
-console.log("final bill is: ", finalBill);
-console.log("your total bill without discount is;", total);
-console.log("name:",name , "tshirt:", item1Price , "shirt:", item2Price , "discount on:", discount , "your total bill:", total , "final pay bill:", finalBill);
-let product = {
-    itemName: "Pen",
-    rate: 99,
-    isAvalibe: true,
-    discount: 9,
-}
-let pay = product.rate - product.discount;
-console.log("details:", product);
-console.log(product.rate);
-console.log("finalpay", pay);
-// create a const object called profile
-const profile = {
-    name: "Gourav",
-    post: 256,
-    follower: 1000,
-    isFollow: false,
-}
-console.log(profile);
-
-//practice java script print and alert
-console.log("welcome");
-console.log("wow how's the css");
-// variables
 name1 = "Gourav in";
 age = 43;
 isFollow = true;
@@ -95,3 +48,14 @@ let name3 = "john";
 let lastname = "dey"
 let fullName1 = name3+" "+lastname;
 console.log("fullname is:", fullName1);
+//today study loop and string 
+for (let a = 1; a <= 999; a++) {
+    console.log("let value 1 =", a);
+}
+//calculate sum 1 to 10 number with varibale
+let sum = 0;
+let x = 999;
+for (let i = 1; i <=x; i++) {
+    sum = sum + i;
+}
+console.log("sum = ", sum);
